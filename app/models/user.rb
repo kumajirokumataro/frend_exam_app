ad2886f1ffaf891c5ece026a037b6525ddd536d3
はらなおみ <haraweb7@gmail.com>
2023-10-13 13:43:33 +0900

    ユーザーアカウント作成後、詳細ページへ遷移する機能完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many : posts
+  has_many :posts
   validates :name, presence: true
   validates :email, presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
